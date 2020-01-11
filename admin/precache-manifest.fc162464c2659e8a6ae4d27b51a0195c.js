e0947d80e116034587d9dcd5bc3ca782065d9d6f
caidi <380141132@qq.com>
2020-01-11 11:49:49 +0800

    :art:修改后台路径为admin

---
+++ admin/precache-manifest.fc162464c2659e8a6ae4d27b51a0195c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a28430d00b8df6387d8a6a52bcb45f",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "388ddc6a95f3c4abac90",
+    "url": "/admin/static/css/4.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "f498f4b8cf96db6a4a79",
+    "url": "/admin/static/css/5.293c32e9.chunk.css"
+  },
+  {
+    "revision": "6edbed4692454903d4fa",
+    "url": "/admin/static/css/6.a9fb7a79.chunk.css"
+  },
+  {
+    "revision": "885e6a2359afc3813d8b",
+    "url": "/admin/static/css/main.663531ec.chunk.css"
+  },
+  {
+    "revision": "1827409456958fd9c7e1",
+    "url": "/admin/static/js/0.4e31e517.chunk.js"
+  },
+  {
+    "revision": "65ec9871343df5aacc52",
+    "url": "/admin/static/js/1.20799d57.chunk.js"
+  },
+  {
+    "revision": "519e3952a43343ad6966",
+    "url": "/admin/static/js/10.83a0367c.chunk.js"
+  },
+  {
+    "revision": "32cd3a1e1e47fadb00f9",
+    "url": "/admin/static/js/11.3f580cd1.chunk.js"
+  },
+  {
+    "revision": "388ddc6a95f3c4abac90",
+    "url": "/admin/static/js/4.11de67f0.chunk.js"
+  },
+  {
+    "revision": "f498f4b8cf96db6a4a79",
+    "url": "/admin/static/js/5.2f0be99e.chunk.js"
+  },
+  {
+    "revision": "6edbed4692454903d4fa",
+    "url": "/admin/static/js/6.4c1935c3.chunk.js"
+  },
+  {
+    "revision": "ec7659aab6990a202c45",
+    "url": "/admin/static/js/7.617607e6.chunk.js"
+  },
+  {
+    "revision": "d413ce4631154b92824c",
+    "url": "/admin/static/js/8.d367f3a3.chunk.js"
+  },
+  {
+    "revision": "461d8a1deb5a051d70cb",
+    "url": "/admin/static/js/9.d5724aa5.chunk.js"
+  },
+  {
+    "revision": "885e6a2359afc3813d8b",
+    "url": "/admin/static/js/main.141efcfc.chunk.js"
+  },
+  {
+    "revision": "20c091ea3fed433b90ad",
+    "url": "/admin/static/js/runtime-main.8af8e2d3.js"
+  },
+  {
+    "revision": "c3bc58529da0ebe6a6c0b1fcfe05127d",
+    "url": "/admin/static/media/js.c3bc5852.jpg"
+  },
+  {
+    "revision": "1672631f1bd0aebe34128cb0978f22e5",
+    "url": "/admin/static/media/timg.1672631f.jpg"
+  }
+]);
